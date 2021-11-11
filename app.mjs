1621825de3059f1415e064fa1edf7969eb05d1f2
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2021-11-12 00:20:44 +0900

    fix:

--- app.mjs
+++ app.mjs
@@ -1,16 +1,16 @@
-import https from 'https';
-import fetch from 'node-fetch';
+import https from "https";
+import fetch from "node-fetch";
 
 const fn = async (keepAlive, cnt) => {
-  console.time(`keepAlive: ${keepAlive}`)
+  console.time(`keepAlive: ${keepAlive}`);
   const agent = new https.Agent({
-    keepAlive
-  });  
-  for await (let v of [...Array(cnt)]) {
-    await fetch('https://randomuser.me/api', { agent });
+    keepAlive,
+  });
+  for await (const _ of [...Array(cnt)]) {
+    await fetch("https://randomuser.me/api", { agent });
   }
   agent.destroy();
-  console.timeEnd(`keepAlive: ${keepAlive}`)
+  console.timeEnd(`keepAlive: ${keepAlive}`);
 };
 
 await fn(true, 10);
